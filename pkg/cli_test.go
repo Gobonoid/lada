21f2d668852364c80495020f73c1aaba89969ff2
Gez Page <gezpage@gmail.com>
2021-05-03 13:04:02 +0100

    AddCommand method, command definition parser (WIP)

--- pkg/cli_test.go
+++ pkg/cli_test.go
@@ -11,3 +11,25 @@ func TestNewCli(t *testing.T) {
 		assert.Equal(t, "cli name", cli.Name)
 	})
 }
+
+func TestAddCommand(t *testing.T) {
+	t.Run("Test can add a CLI command", func(t *testing.T) {
+		var cli = NewCli("cli name", "1.0.0")
+
+		commandString := "subcommand $input $output --num=1 --verbose? --flavour= --word=default --flag?"
+		command := cli.AddCommand(commandString, func(args Arguments, params Parameters, flags Flags) {
+			//assert.Len(t, args, 2)
+			//assert.Len(t, params, 3)
+			//assert.Len(t, flags, 2)
+			cli.Output().Write([]byte("Test command"))
+		})
+		err := command.Run()
+		assert.Equal(t, nil, err)
+	})
+}
+
+func TestParse(t *testing.T) {
+	commandString := "subcommand adsasd $input $output --num=1 --flavour= --word=default --verbose? --flag?"
+	tokens := Parse(commandString)
+	assert.Len(t, tokens, 9)
+}
